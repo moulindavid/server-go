25774376b7b4ad30da73f51181917053fd6291ad
moulindavid <moulin.david@live.fr>
2023-07-15 16:01:46 +0200

    feat: middleware and file serving

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
+	cfg := &apiConfig{fileserverHits: 0}
 	mux := http.NewServeMux()
+	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("/healthz", serveHealth)
+	mux.HandleFunc("/metrics", cfg.serveMetrics)
 	corsMux := middlewareCors(mux)
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, corsMux))
@@ -22,6 +29,30 @@ func middlewareCors(next http.Handler) http.Handler {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func serveHealth(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) serveMetrics(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
 		next.ServeHTTP(w, r)
 	})
 }
